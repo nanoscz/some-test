abe5b37fb3fe1717a701e895e7f0b00c8d313a93
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-06 04:47:12 -0400

    Fix lint module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,30 +1,30 @@
 import { NgModule } from '@angular/core';
 
 import {
-	MatToolbarModule,
-	MatIconModule,
-	MatSidenavModule,
-	MatListModule,
-	MatButtonModule,
-	MatCardModule,
-	MatInputModule
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule
  } from '@angular/material';
 
 import { LayoutModule } from '@angular/cdk/layout';
 
 const modules = [
-	MatToolbarModule,
-	MatIconModule,
-	MatSidenavModule,
-	MatListModule,
-	MatButtonModule,
-	LayoutModule,
-	MatCardModule,
-	MatInputModule
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
+  LayoutModule,
+  MatCardModule,
+  MatInputModule
 ];
 
 @NgModule({
-	imports: modules,
-	exports: modules,
+  imports: modules,
+  exports: modules,
 })
 export class MaterialModule { }
