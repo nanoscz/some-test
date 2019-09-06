12e1202b8ac9ed6c5443be1a3178259bbd2aa564
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-06 17:20:56 -0400

    Add logout

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, share } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-main-nav',
@@ -9,13 +12,25 @@ import { map, share } from 'rxjs/operators';
   styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent {
-
+  isLogged: boolean;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       share()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public localStorage: LocalStorageService,
+    public authService: AuthService,
+    public router: Router,
+  ) {
+    this.isLogged = this.authService.isLogged()
+  }
 
+  logout() {
+    this.localStorage.delete('token');
+    this.localStorage.delete('account');
+    this.router.navigate(['/login']);
+  }
 }
