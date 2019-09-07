8c943afa1e8f68c3a5d831128f0b9f82c814d7d9
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-07 02:11:15 -0400

    Add template questionnaire (dashboard)

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  items = [1, 2, 3, 4, 5];
   constructor() { }
 
   ngOnInit() {
