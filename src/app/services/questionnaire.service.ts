3e2b089bc86a317a1033e4681e451c5ecd21bbee
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-12 02:12:58 -0400

    Add questionnaire service

---
+++ src/app/services/questionnaire.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionnaireService {
+  baseUrl = '/v1/questionnaire';
+  constructor(public http: HttpClient) { }
+
+  findOne(uuidTest: string) {
+    return this.http.get(`${this.baseUrl}/${uuidTest}`)
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  save(questionnaire: string) {
+    return this.http.post(this.baseUrl, questionnaire)
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  handleError(err: any): Promise<any> {
+    return Promise.reject(err.error);
+  }
+
+}
