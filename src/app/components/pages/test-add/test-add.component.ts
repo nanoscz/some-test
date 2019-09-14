55d33b1fe66707ddaae5a93952426c239ef1beea
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-14 15:50:21 -0400

    Save questionnaire

--- src/app/components/pages/test-add/test-add.component.ts
+++ src/app/components/pages/test-add/test-add.component.ts
@@ -2,26 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
+/** Services */
+import { TestService } from 'src/app/services/test.service';
+import { QuestionService } from 'src/app/services/question.service';
+import { QuestionnaireService } from 'src/app/services/questionnaire.service';
+import { MatSnackBar } from '@angular/material';
+
 @Component({
   selector: 'app-test-add',
   templateUrl: './test-add.component.html',
   styleUrls: ['./test-add.component.scss']
 })
 export class TestAddComponent implements OnInit {
-  public answers = {
-    text: '',
-    point: 0,
-    question_id: null
-  };
-
-  public questions = {
-    query: '',
-    multiple: false
-  };
-
   form: FormGroup;
 
-  constructor(public router: Router, private fb: FormBuilder) { }
+  constructor(
+    public router: Router,
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private testService: TestService,
+    private questionService: QuestionService,
+    private quesionnaireService: QuestionnaireService
+  ) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -30,14 +32,55 @@ export class TestAddComponent implements OnInit {
       type: ['normal', [Validators.required]],
       questions: this.fb.array([])
     });
-
-    this.addQuestion();
   }
 
-  onSubmit() {
+  async onSubmit() {
     this.form.markAllAsTouched();
+    if (!this.form.valid) {
+      return;
+    }
     const formData = this.form.value;
-    console.log(formData);
+    const { name, type, questions } = formData;
+    if (!questions.length) {
+      this.showMessage('You must add questions ot the questionnaire.', 2000);
+      return;
+    }
+    const user_id = this.getUserId();
+    const test = { name, type, user_id };
+    const response = await this.testService.save(test).catch(this.handleError);
+    const testId = response[0].id;
+    const questionsPromise = [];
+    for (const question of questions) {
+      if (!question.answers.length) {
+        this.showMessage('You must add answers to the questions.', 2000);
+        questionsPromise.length = 0;
+        return;
+      }
+      const { query, multiple, answers } = question;
+      const objQuestion = { query, multiple, answers: JSON.stringify(answers) };
+      questionsPromise.push(this.questionService.save(objQuestion));
+    }
+    Promise.all(questionsPromise).then(async responseQuestions => {
+      const arrQuestionnaire = [];
+      for (const question of responseQuestions) {
+        arrQuestionnaire.push({
+          testId,
+          questionId: question.id
+        });
+      }
+      const questionnaire = JSON.stringify(arrQuestionnaire);
+      await this.quesionnaireService.save({ questionnaire }).catch(this.handleError);
+      this.showMessage('Questionnaire created correctly.', 2500);
+      this.toBack();
+    });
+  }
+
+  getUserId() {
+    return 1;
+  }
+
+  showMessage(msg: string, duration: number) {
+    this._snackBar.open(msg, null, { duration });
   }
 
   getErrorMessage(type) {
@@ -49,6 +92,7 @@ export class TestAddComponent implements OnInit {
     const questions = <FormArray>this.form.controls['questions'];
     questions.removeAt(index);
   }
+
   addQuestion() {
     const questions = <FormArray>this.form.controls['questions'];
     const newGroup = this.fb.group({
@@ -62,4 +106,10 @@ export class TestAddComponent implements OnInit {
   toBack() {
     this.router.navigate(['/dashboard/test']);
   }
+
+  handleError(err: any): Promise<any> {
+    return Promise.reject(err);
+  }
+
 }
+
