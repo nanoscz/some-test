4a2b432d420d4c3127b9455dd9a69bfcab70b85b
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-15 01:19:37 -0400

    Remove line unnecessary

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Test } from '../interfaces/resources.interface';
 
 @Injectable({
   providedIn: 'root'
