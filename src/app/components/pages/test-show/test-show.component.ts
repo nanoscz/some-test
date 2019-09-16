ff19ac24c744615f3e4dc3d59ffc13ce52f3e984
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-16 01:27:49 -0400

    Add toDelete method (test)

--- src/app/components/pages/test-show/test-show.component.ts
+++ src/app/components/pages/test-show/test-show.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { QuestionnaireService } from 'src/app/services/questionnaire.service';
+import { TestService } from 'src/app/services/test.service';
 
 @Component({
   selector: 'app-test-show',
@@ -10,19 +11,29 @@ import { QuestionnaireService } from 'src/app/services/questionnaire.service';
 })
 export class TestShowComponent implements OnInit {
   public test: any = null;
+  public testId: number;
   constructor(
     private router: Router,
     private questionnaireService: QuestionnaireService,
+    private testService: TestService,
     private activatedRoute: ActivatedRoute
   ) {
     this.activatedRoute.params.subscribe(async params => {
-      console.log(params.id)
-      this.test = await this.questionnaireService.findOne(params.id).catch(this.handleError);
+      this.testId = params.id;
+      this.test = await this.questionnaireService.findOne(this.testId).catch(this.handleError);
       console.log(this.test);
     });
   }
 
   ngOnInit() {
+  }
+
+  toDetele() {
+    this.testService.delete(this.testId)
+      .then(() => {
+        this.toBack();
+      })
+      .catch(this.handleError);
   }
 
   toBack() {
